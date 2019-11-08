79179dcc20f6f194419bfbc1c1946c0972e93d82
liujiadong <liujiadong@xunlei.com>
2019-11-08 17:18:02 +0800

    动态权限获取

--- app/src/main/java/com/termux/test/TermuxActivity.java
+++ app/src/main/java/com/termux/test/TermuxActivity.java
@@ -1,16 +1,23 @@
 package com.termux.test;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.termux.R;
 import com.termux.TermuxHelper;
 import com.termux.Termux;
-import com.termux.TermuxListener;
 import com.termux.terminal.TermuxDebug;
 
 
@@ -31,7 +38,30 @@ public class TermuxActivity extends Activity {
 
         textView = findViewById(R.id.text);
 
-        TermuxHelper.initTermux(this);
+        if (hasPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            TermuxHelper.initTermux(this);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
+        }
+    }
+
+    public static boolean hasPermission(@NonNull Context context, @NonNull String permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[]
+            grantResults) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && requestCode == 200) {
+            if (hasPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                TermuxHelper.initTermux(this);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
+            }
+        }
     }
 
     public void btn1(View view) {
